eee0c51e13e13b374a7bc9df98a4def7ca95b5bc
Ravindu Deshan <Droidcasts@protonmail.com>
2024-02-11 10:59:06 +0530

    SELinux : Force Permissive

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -180,6 +180,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	selnl_notify_setenforce(new_value);
 	selinux_status_update_setenforce(state, new_value);
 #else
+	new_value = 0;
 	if (new_value != selinux_enforcing) { // SEC_SELINUX_PORTING_COMMON Change to use RKP
 		length = avc_has_perm(&selinux_state,
 				      current_sid(), SECINITSID_SECURITY,
